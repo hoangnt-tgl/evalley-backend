edfb3054f63fb7a6217811dbb7085182cbb9a7d0
tronghoangbk <hoang.nguyentrong197@gmail.com>
2022-06-25 00:00:36 +0700

    1

---
+++ models/review.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose")
+// Voucher Schema
+var ReviewSchema = mongoose.Schema({
+    product_id: {
+        type: String
+    },
+    username: {
+        type: String
+    },
+    message: {
+        type: String
+    },
+    createdAt: {
+        type: Date
+    },
+    reply: [
+        {
+            username: String,
+            createdAt: Date,
+            message: String
+        }
+    ]
+});
+
+var Review = module.exports = mongoose.model('review', ReviewrSchema);
+
+
+
+
