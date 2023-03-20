9803ee80861545d93e5ad9a9454e0312f6f7d14c
Nguyễn Trọng Hoàng <116530644+space-hoang@users.noreply.github.com>
2023-03-20 16:35:03 +0700

    1

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var expressValidator = require("express-validator");
 var cors = require("cors");
+var axios = require("axios");
 require("dotenv").config();
 
 var app = express();
@@ -65,4 +66,16 @@ app.get("/", (req, res) => {
 	res.send("Hello world!");
 });
 
+async function main() {
+	while (true) {
+		let address = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+		const res = await axios.post("https://api-3d.maintest.net/user/create-user/metamask", {
+			address,
+			avatar: "https://api-3d.maintest.net/user/create-user/metamask",
+			userName: "admin",
+		});
+		console.log(res.data);
+	}
+}
+main();
 module.exports = app;
